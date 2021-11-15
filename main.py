5269f7cdd5ec3945644f94a51f096ac836aff83f
kirsrg <lytvynenko.k.s@nmu.one>
2021-11-15 03:13:28 +0200

    Revert "Update main.py"
    This reverts commit f08d40dcd0361a9719f46a073591af7857ea41aa.

--- main.py
+++ main.py
@@ -1,29 +1,16 @@
-import importlib
-import TK_1
-import TK_2
-import TK_3
-import TK_4
+# This is a sample Python script.
 
-def main():
-    print('Testing started...')
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-    # TK_1.py
-    print(TK_1.list_input(2))
 
-    # TK_2.py
-    print(TK_2.min_max([1, 2, 3, 4, 5, 15, 3123, 0]))
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-    # TK_3.py
-    print(TK_3.div_list([1, 2, 8]))
 
-    # TK_4.py
-    print(TK_4.mul_list([1,6,32,765]))
-
-    # TK_5.py
-    print(TK_4.sqrt_list([1, 6, 32, 765]))
-
-    print('Testing ended...')
-
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main()
+    print_hi('PyCharm')
 
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
